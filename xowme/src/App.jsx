3d4a834d23b6327dde29816df67dbd5621012566
Aymen Hassini <hassiniaymen@yahoo.com>
2026-02-04 07:44:23 +0100

    adding routing

--- xowme/src/App.jsx
+++ xowme/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+//import NavigationBar from "./components/NavigationBar.jsx";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      {/* <NavigationBar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
